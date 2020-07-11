389f9be02090ac0237bb9501cb04f6131abe1b35
high-moctane <high.moctane@gmail.com>
2020-07-12 02:39:12 +0900

    Fixed image name and workdir

--- Ruby_MRI/docker.sh
+++ Ruby_MRI/docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-IMAGE_TAG=anylang-rl-python3_pypy3
+IMAGE_TAG=anylang-rl-ruby_mri
 
 docker build -t ${IMAGE_TAG} .
 docker run \
     --rm \
     -it \
     --mount type=bind,src=${SCRIPT_DIR}/../,dst=/anylang-rl \
-    -w /anylang-rl/Python3_PyPy3 \
+    -w /anylang-rl/Ruby_MRI \
     ${IMAGE_TAG} \
     ${@}
